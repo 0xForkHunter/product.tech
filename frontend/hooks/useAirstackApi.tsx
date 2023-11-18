5b39c1eee850ee27599785e827fd20dc404c149f
0xForkHunter <0xforkhunter@protonmail.com>
2023-11-18 17:52:14 +0300

    fix wrong imports

--- frontend/hooks/useAirstackApi.tsx
+++ frontend/hooks/useAirstackApi.tsx
@@ -1,14 +1,14 @@
 import { AirstackSocialProfiles } from "@/lib/api/backend/airstack";
 import { ApiResponse } from "@/models/apiResponse.model";
-import { SimpleUseQueryOptions } from "@/models/helpers.model";
 import { useQuery } from "@tanstack/react-query";
 import { useAxios } from "./useAxios";
 
-export const useGetWalletSocials = (address?: `0x${string}`, options?: SimpleUseQueryOptions) => {
+export const useGetWalletSocials = (address?: `0x${string}`, options?: { enabled?: boolean }) => {
   const axios = useAxios();
   return useQuery({
     queryKey: ["useGetWalletSocials", address],
     enabled: !!address,
+    ...options,
     queryFn: () =>
       axios
         .get<ApiResponse<AirstackSocialProfiles>>("/api/airstack/social", { params: { address } })
