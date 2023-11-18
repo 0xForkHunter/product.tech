8f2e6dc1d3c043feca4afe479de5db811e2308da
0xForkHunter <0xforkhunter@protonmail.com>
2023-11-18 21:07:16 +0300

    update contract address

--- frontend/data/constants.tsx
+++ frontend/data/constants.tsx
@@ -5,7 +5,7 @@ export const APP_LOGO = "/logo.png";
 export const DEFAULT_PROFILE_PICTURE = "/default-profile.png";
 export const BASE_API_URL = "https://product-tech-5c84bae0fb08.herokuapp.com";
 export const MAIN_COLOR = "#FF6154";
-export const PRODUCT_TECH_ADDRESS = "0xE60f5Ef3E822D4a46620ba2e382b752941214eca";
+export const PRODUCT_TECH_ADDRESS = "0x277b12B641f28B7Bb24601072D34fc48c706af06";
 export const PRODUCT_TECH_CONTRACT = {
   address: PRODUCT_TECH_ADDRESS,
   abi: PRODUCT_TECH_ABI,
