5105700d0a8249e4f057b47e48bd67687dc03aa5
0xForkHunter <0xforkhunter@protonmail.com>
2023-11-19 01:01:10 +0300

    Circumvent the safe creation bug

--- frontend/hooks/useSafe.tsx
+++ frontend/hooks/useSafe.tsx
@@ -5,9 +5,10 @@ import { ethers } from "ethers";
 import { SafeFactory, EthersAdapter, SafeAccountConfig } from "@safe-global/protocol-kit";
 import { Button } from "@ensdomains/thorin";
 import { CircularProgress } from "@mui/material";
-import {  useNetwork } from "wagmi";
+import { useNetwork } from "wagmi";
 import { useEthersSigner } from "@/hooks/useEthersSigner";
 import { PRODUCT_TECH_ADDRESS } from "@/data/constants";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 
 export const useCreateSafe = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
@@ -25,8 +26,16 @@ export const useCreateSafe = () => {
     });
 
     const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapter });
+
+    //There seems to be a bug in the protocol that prevents creating multiple safes for the same owner address.
+    //We generate a new address to circumvent this issue for the sake of the demo.
+    //The owner address should normally be the contract
+
+    const privateKey = generatePrivateKey();
+    const account = privateKeyToAccount("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+
     const safeAccountConfig: SafeAccountConfig = {
-      owners: [PRODUCT_TECH_ADDRESS],
+      owners: [account.address],
       threshold: 1,
     };
 
